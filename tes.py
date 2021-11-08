9f8541f3685d1a3941a22b70119dd34b8546a17a
bitCoinIsGod <2bh0104@gmail.com>
2021-11-08 16:33:55 +0900

    딕셔너리

--- tes.py
+++ tes.py
@@ -1,10 +1,3 @@
-portfolio = []
-
-portfolio.append("BTC")
-portfolio.append("SOL")
-portfolio.append("ETH")
-
-portfolio.insert(1,"dsadREP")
-portfolio[0] = "ccc"  
-
-print(portfolio[:5])  
+prices = {'BTC': 8300000, 'XRP': 514}
+prices['ETH'] = 600000
+print(prices)
