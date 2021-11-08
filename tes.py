ef324f5a91d4bdc241827d34f418dfe6e037734e
bitCoinIsGod <2bh0104@gmail.com>
2021-11-08 16:54:08 +0900

    1주차 딕셔너리까지..

--- tes.py
+++ tes.py
@@ -1,3 +1,18 @@
-prices = {'BTC': 8300000, 'XRP': 514}
-prices['ETH'] = 600000
-print(prices)
+repple = [800,900]
+repple.append(950)
+repple.append(970)
+repple.append(980)
+
+print(repple)
+
+repple_dit = {}
+
+repple_dit["2/21"] = repple[0]  
+repple_dit["2/22"] = repple[1]
+repple_dit["2/23"] = repple[2]
+repple_dit["2/24"] = repple[3]
+repple_dit["2/25"] = repple[4]
+
+avr  = sum(list(repple_dit.values())) / len(list(repple_dit.values()))
+
+print(avr)
