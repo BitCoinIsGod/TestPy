16a06323f7d93dd6306776e4e31739e55b13b94a
bitCoinIsGod <2bh0104@gmail.com>
2021-11-09 10:33:21 +0900

    반복

--- tes.py
+++ tes.py
@@ -1,18 +1,3 @@
-repple = [800,900]
-repple.append(950)
-repple.append(970)
-repple.append(980)
-
-print(repple)
-
-repple_dit = {}
-
-repple_dit["2/21"] = repple[0]  
-repple_dit["2/22"] = repple[1]
-repple_dit["2/23"] = repple[2]
-repple_dit["2/24"] = repple[3]
-repple_dit["2/25"] = repple[4]
-
-avr  = sum(list(repple_dit.values())) / len(list(repple_dit.values()))
-
-print(avr)
+ripple = [511, 516, 508, 505, 503]
+for close in ripple:         # ripple 리스트 안에 있는 close에 대해서 
+    print(close)
