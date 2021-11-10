d9b159d54222ff5a27568de4d39e0b95e84eed2b
bitCoinIsGod <2bh0104@gmail.com>
2021-11-10 10:41:59 +0900

    클래스

--- tes.py
+++ tes.py
@@ -1,7 +1,11 @@
-class MyClass:
-    def add(self, a, b):
-        return a + b
+class book:
+    def __init__(self,책제목,저자,역자,출판사,ISB10):
+        self.책제목 = 책제목
+        self.저자 = 저자
+        self.역자 = 역자
+        self.출판사 = 출판사
+        self.ISB10 = ISB10
 
-obj = MyClass()
-ret = obj.add(3, 4)
-print(ret)
+
+book1 = book("헴","저자1","역자1","출판사1","ISB10")
+print(book1.책제목)
